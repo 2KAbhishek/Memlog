bbb8f75071128d7cd936f0d405da38946505ef72
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-04-12 18:01:18 +0530

    Add next and prev functionality.

--- app/src/main/java/com/iam2kabhishek/memlog/EditMemory.kt
+++ app/src/main/java/com/iam2kabhishek/memlog/EditMemory.kt
@@ -31,19 +31,31 @@ class EditMemory : AppCompatActivity() {
 
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_next -> true
+            R.id.action_next -> {
+                moveNext()
+                true
+            }
+            R.id.action_previous -> {
+                movePrevious()
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun moveNext() {
+        ++memoryPosition
+        displayMemory()
+    }
+    private fun movePrevious() {
+        --memoryPosition
+        displayMemory()
     }
 
     private fun displayMemory() {
