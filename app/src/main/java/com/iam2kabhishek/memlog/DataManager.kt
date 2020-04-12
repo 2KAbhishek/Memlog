df4dc82ee65ff24af58010608cac914b4419b3b5
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-04-12 15:31:33 +0530

    Add initNotes()

--- app/src/main/java/com/iam2kabhishek/memlog/DataManager.kt
+++ app/src/main/java/com/iam2kabhishek/memlog/DataManager.kt
@@ -6,12 +6,21 @@ class DataManager {
 
     init {
         initMoods()
+        initNotes()
     }
     
     private fun initMoods(){
         var mood = MoodInfo("happy", "☺ Happy ☺")
         moods[mood.moodId] = mood
-        mood = MoodInfo("sad", "☹ Sad ☹")
+        mood = MoodInfo("sad", "☹️ Sad ☹️")
         moods[mood.moodId] = mood
+    }
+
+    private fun initNotes(){
+        var mood = MoodInfo("happy", "☺ Happy ☺")
+        var memory = MemoryInfo(mood, "Title1", "This is a memory")
+        mood = MoodInfo("sad", "☹️ Sad ☹️")
+        memory = MemoryInfo(mood, "Title2", "This is a memory")
+        memory = MemoryInfo(mood, "Title3", "This is a memory")
     }
 }
