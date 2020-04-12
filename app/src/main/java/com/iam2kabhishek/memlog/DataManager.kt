15df5ef2c22fbc2250ef8d69ec242d9b8389190f
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-04-12 11:44:04 +0530

    Add initMoods

--- app/src/main/java/com/iam2kabhishek/memlog/DataManager.kt
+++ app/src/main/java/com/iam2kabhishek/memlog/DataManager.kt
@@ -3,4 +3,12 @@ package com.iam2kabhishek.memlog
 class DataManager {
     val moods = HashMap<String, MoodInfo>()
     val logs = ArrayList<LogInfo>()
+
+    
+    private fun initMoods(){
+        var mood = MoodInfo("☺", "Happy")
+        moods[mood.moodId] = mood
+        mood = MoodInfo("☹", "Sad")
+        moods[mood.moodId] = mood
+    }
 }
