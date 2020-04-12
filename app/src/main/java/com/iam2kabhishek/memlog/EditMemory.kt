1fd7872df8164dd5f8e8bf3b968792d8bf3372f8
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-04-12 17:25:18 +0530

    Add menu methods.

--- app/src/main/java/com/iam2kabhishek/memlog/EditMemory.kt
+++ app/src/main/java/com/iam2kabhishek/memlog/EditMemory.kt
@@ -1,6 +1,8 @@
 package com.iam2kabhishek.memlog
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 
 import kotlinx.android.synthetic.main.activity_edit_memory.*
@@ -25,6 +27,23 @@ class EditMemory : AppCompatActivity() {
 
         if(memoryPosition != POSITION_NOT_SET)
             displayMemory()
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_next -> true
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     private fun displayMemory() {
