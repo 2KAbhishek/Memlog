f5a8247c00437dd15c1d3b240b4597fd86ae9387
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-04-25 16:09:09 +0530

    Add nested ViewHolder.

--- app/src/main/java/com/iam2kabhishek/memlog/MemoryAdapter.kt
+++ app/src/main/java/com/iam2kabhishek/memlog/MemoryAdapter.kt
@@ -1,4 +1,27 @@
 package com.iam2kabhishek.memlog
 
-class MemoryAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MemoryAdapter(private val context: Context) : RecyclerView.Adapter<MemoryAdapter.ViewHolder>() {
+
+    private val layoutInflater = LayoutInflater.from(context)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
 }
