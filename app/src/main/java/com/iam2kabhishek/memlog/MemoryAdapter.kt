b75391e0039cc94e33439a1b4f0e83f861ba468d
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-04-25 17:14:23 +0530

    Add MemoryAdapter override definitions

--- app/src/main/java/com/iam2kabhishek/memlog/MemoryAdapter.kt
+++ app/src/main/java/com/iam2kabhishek/memlog/MemoryAdapter.kt
@@ -4,24 +4,30 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class MemoryAdapter(private val context: Context) : RecyclerView.Adapter<MemoryAdapter.ViewHolder>() {
+class MemoryAdapter(private val context: Context, private val memories: List<MemoryInfo>) :
+    RecyclerView.Adapter<MemoryAdapter.ViewHolder>() {
 
     private val layoutInflater = LayoutInflater.from(context)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val itemView = layoutInflater.inflate(R.layout.item_memory_list, parent, false)
+        return ViewHolder(itemView)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount() = memories.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val memory = memories[position]
+        holder.textMood?.text = memory.mood?.name
+        holder.textTitle?.text = memory.title
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val textMood = itemView.findViewById<TextView?>(R.id.textMood)
+        val textTitle = itemView.findViewById<TextView?>(R.id.textMemTitle)
+    }
 
 }
